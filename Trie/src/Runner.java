b4e5afa7d37a084a154872d784649860c31fd983
Surya Sikha Rej <46563058+iamsuryasikharej@users.noreply.github.com>
2019-12-22 18:47:24 +0530

    Update Runner.java

--- Trie/src/Runner.java
+++ Trie/src/Runner.java
@@ -5,12 +5,7 @@ public static void main(String[] args) {
 	Trie t=new Trie();
 	t.add("surya");
 	t.add("si");
-//	t.add("sude");
-//	t.add("zhg");
-	Node temp=t.rootNode;
-
-	
-		System.out.println(temp.child.get('s').child.get('i').isword);
+	System.out.println(temp.child.get('s').child.get('i').isword);
 //	System.out.println(temp.child.keySet());
 //	System.out.println(temp.child.get('z').child.get('h').child.get('g').isword);
 	
